ca53084654f37785584d7c15f4a4520536587d62
JerryK026 <kskyung0624@gmail.com>
2023-10-10 09:32:44 +0900

    boj2018 pass

---
+++ java/src/main/java/org/boj/Boj2018.java
@@ -0,0 +1,32 @@
+package org.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj2018 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        int start = 1;
+        int end = 1;
+        int sum = 1;
+        while (start <= end) {
+            if (sum == n) {
+                cnt++;
+                end++;
+                sum += end;
+            } else if (sum > n) {
+                sum -= start;
+                start++;
+            } else {
+                end++;
+                sum += end;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
